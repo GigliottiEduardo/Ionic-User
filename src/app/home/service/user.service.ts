08966487d5ff49ba31f6d5a9579030493efe5c25
Eduardo Gigliotti <99850450+GigliottiEduardo@users.noreply.github.com>
2023-04-14 21:28:27 -0300

    primeiro commit

---
+++ src/app/home/service/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Data } from '../models/data.model';
+import { Support } from '../models/support.model';
+@Injectable({
+    providedIn: 'root'
+})
+export class UsersService {
+    url = 'https://reqres.in/api/users/2'
+
+    constructor(private http: HttpClient) { }
+
+    create(data: Data, support: Support) {
+        return this.http.post(this.url, Data);
+    }
+
+    getAll() {
+        return this.http.get(this.url);
+    }
+
+    getOne(id: number) {
+        return this.http.get(`${this.url} / ${id}}`);
+    }
+
+    update(data: Data) {
+        return this.http.put(`${this.url} / ${data.id}`, data);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${this.url}/${id}`);
+    }
+}
